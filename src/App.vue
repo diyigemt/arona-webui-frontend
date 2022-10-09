8bdeab01748d070af8ea0530941e9a6fc86ff37d
diyigemt <qwe1355247243@gmail.com>
2022-10-09 12:10:41 +0800

    init: init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import zhCn from "element-plus/lib/locale/lang/zh-cn";
+
+const locale = zhCn;
+</script>
+
+<template>
+  <div class="p-5">
+    <el-config-provider :locale="locale">
+      <router-view></router-view>
+    </el-config-provider>
+  </div>
+</template>
---
+++ src/views/home/HomeIndex.vue
@@ -0,0 +1,14 @@
+<template>
+  <TimeLine />
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.time-item {
+  display: inline-block;
+  height: 200px;
+  min-width: var(--base-width);
+  background-color: #00000030;
+}
+</style>
